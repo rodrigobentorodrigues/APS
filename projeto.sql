e1bc0329528c2d66d1ff5f7bdbb36c4d3aba8d09
Rodrigo Bento <rodrigobentor2014@gmail.com>
2017-03-05 21:20:11 -0300

    Script criado

---
+++ projeto.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE funcionario (
+	nome varchar(40) not null,
+	sexo varchar(10) not null,
+	habilitacao varchar(20) not null,
+	idade int not null,
+	email varchar(40) not null,
+	senha varchar(20) not null,
+	id SERIAL primary key
+)
+
+CREATE TABLE doador (
+	nome varchar(40) not null,
+	cpf varchar(15) not null unique,
+	dataNascimento varchar(10) not null,
+	sexo varchar(10) not null,
+	tipoSanguineo varchar(5) not null,
+	peso double precision not null,
+	id SERIAL primary key
+)
